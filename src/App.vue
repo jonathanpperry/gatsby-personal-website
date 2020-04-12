211d5a07ed7b20ce1d1cda7114ca25dafc90208b
Jonathan Perry <jpper@umich.edu>
2020-04-12 18:39:45 +0900

    Renamed the hello world component to home, updated the toolbar avatar, and added a router

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,26 @@
   <v-app>
     <v-app-bar app color="white" height="100">
       <v-avatar class="mr-3" color="grey lighten-5" size="70">
-        <v-img
-          contain
-          max-height="70%"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-        ></v-img>
+        <v-img src="./assets/john.png" contain max-height="70%"></v-img>
       </v-avatar>
 
       <v-toolbar-title class="font-weight-black headline">Jonathan Perry's Website</v-toolbar-title>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld />
+      <Home />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import Home from "./components/Home";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld
+    Home
   },
 
   data: () => ({
